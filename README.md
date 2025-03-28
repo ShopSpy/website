ce5dde7afcbec3938db67b4bc9262f8191e82d9e
Jake Nicholls <git@jakenicholls.au>
2025-03-28 12:10:22 +1100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ShopSpy Website
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<div class="flex h-screen flex-col items-center justify-center gap-6 bg-black text-white">
+	<h1 class="text-6xl font-bold">ShopSpy</h1>
+	<p class="text-center text-xl">
+		The simplest way to compare supermarket prices and find<br /> the best deals, saving you more on
+		every shop.
+	</p>
+	<p class="text-2xl italic">Coming soon...</p>
+	<div class="flex flex-row gap-2">
+		<a href="mailto:hello@shopspy.au" class="">hello&#64;shopspy&#46;au</a>
+		<p>|</p>
+		<a href="https://github.com/ShopSpy" class="underline">GitHub</a>
+	</div>
+</div>
