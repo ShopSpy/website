a8c6f5757baca9080a8b269e351422ebf2411c3c
Jake Nicholls <git@jakenicholls.au>
2025-04-04 22:24:38 +1100

    Fix posthog + add colours and font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
 	import '../app.css';
+	import { onMount } from 'svelte';
+	import posthog from 'posthog-js';
+	import { browser } from '$app/environment';
 
+	onMount(() => {
+		if (browser) {
+			posthog.init('phc_VVNt8OcoqeDHu5vyh0JjblIG5QhouCR9IkFVtQtxnNc', {
+				api_host: 'https://us.i.posthog.com',
+				person_profiles: 'identified_only'
+			});
+		}
+	});
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<title>ShopSpy</title>
 </svelte:head>
-{@render children()}
+<div class="font-display selection:bg-shopspy-200 bg-neutral-100">
+	{@render children()}
+</div>
